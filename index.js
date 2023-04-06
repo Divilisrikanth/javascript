def765112a6a1258495a6783401c99da5c4ff110
Divilisrikanth <srikanthdivili1994@gmail.com>
2023-04-06 18:28:26 +0530

    pushing this into my github repo

---
+++ index.js
@@ -0,0 +1,67 @@
+// variables
+const pi ="3.14";
+console.log(pi);
+var a = "something";
+console.log(a);
+console.log(b);
+//console.log(c)
+var b ="hello world";
+//let c ="hi everyone";
+//javascript executes line by line if there is any error, For example line no 7 as error it will stop execution on line 7
+//line 12 and 13 will not  be executed
+const pii ="3.14";
+console.log(pii);
+//cannot redeclare the variable which are defined using let keyword
+//let d="256";
+//let d=257;
+
+//variables declared using var keyword
+var e = "259"
+e=260
+console.log(e)
+
+//variables declared using const keyword
+//const f=261;
+//const f =262;
+//console.log()
+//Assignment operators
+let x=5;
+let y=6;
+console.log(x+y)
+console.log(x-y)
+console.log(x/y)
+console.log(x%y)
+console.log(x--)
+console.log(x++)
+let p =2
+let q=3;
+let r =q+=p;
+console.log(r)
+console.log(q+=p)
+console.log(q*=p)
+console.log(q/=p)
+console.log(q%=p)
+console.log(q**=p)
+//shift assignment operators
+g=12
+h=15
+console.log(g<<=h)
+console.log(g>>=h)
+console.log(g>>>=h)
+//logical operators
+m=0;
+n=1;
+console.log(m&&n)
+console.log(m||n)
+let x1=2;
+let y1="abc";
+let z1=2;
+console.log(x1==z1)
+console.log(x1===z1)
+console.log(z1==y1)
+console.log(x1==y1)
+console.log(x1===y1)
+
+
+
+
